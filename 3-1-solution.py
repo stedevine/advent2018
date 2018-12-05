5cc53e09f4c9fcd8fc4e63488859e04f8c0b9127
Stephen Devine <stephen.devine@bigfishgames.com>
2018-12-04 17:20:28 -0800

    3-1 idea

---
+++ 3-1-solution.py
@@ -0,0 +1,12 @@
+# For a large area out of which claims can be made
+# Count the total area that is claimed more than once
+
+# Consider an NxM array of states:
+# Unclaimed
+# Claimed
+# Claimed More than once
+
+# Figure out the maxium size required
+# Create the board
+# Apply each claim to the board
+#   Flip the states (Unclaimed -> Claimed, Claimed -> Claimed More Than Once, Claimed More than once stays the same)
